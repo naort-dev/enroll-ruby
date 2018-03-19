ideacrew/enroll

Refs #22978 removed autobuild since it doesn't work with polymorphism

components/sponsored_benefits/app/models/sponsored_benefits/benefit_markets/benefit_market.rb
@@ -16,7 +16,7 @@ module SponsoredBenefits
       has_many    :benefit_applications,  class_name: "SponsoredBenefits::BenefitApplications::BenefitApplication"
       has_many    :benefit_catalogs,      class_name: "SponsoredBenefits::BenefitCatalogs::BenefitCatalog"
 
-      embeds_one :configuration_setting,  as: :configurable, autobuild: true
+      embeds_one :configuration_setting,  as: :configurable
       # embeds_one :contact_center_setting, class_name: "SponsoredBenefits::BenefitMarkets::ContactCenterConfiguration",
       #                                     autobuild: true
 

naort-dev/enroll-ruby

components/sponsored_benefits/app/models/sponsored_benefits/benefit_markets/benefit_market.rb
@@ -19,7 +19,7 @@ module SponsoredBenefits
       has_many    :benefit_applications,  class_name: "SponsoredBenefits::BenefitApplications::BenefitApplication"
       has_many    :benefit_catalogs,      class_name: "SponsoredBenefits::BenefitCatalogs::BenefitCatalog"
 
-      embeds_one :configuration,  as: :configurable, autobuild: true
+      embeds_one :configuration,  as: :configurable
       # embeds_one :contact_center_setting, class_name: "SponsoredBenefits::BenefitMarkets::ContactCenterConfiguration",
       #                                     autobuild: true
 