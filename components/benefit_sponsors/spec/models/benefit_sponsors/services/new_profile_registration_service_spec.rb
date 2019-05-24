ideacrew/enroll

refs #39064 permission for broker staff role to edit broker ageency profile

components/benefit_sponsors/spec/models/benefit_sponsors/services/new_profile_registration_service_spec.rb
@@ -10,12 +10,13 @@ module BenefitSponsors
     let!(:site) { ::BenefitSponsors::SiteSpecHelpers.create_cca_site_with_hbx_profile_and_benefit_market }
     let!(:general_org) {FactoryGirl.create(:benefit_sponsors_organizations_general_organization, :with_aca_shop_cca_employer_profile, site: site)}
     let!(:employer_profile) {general_org.employer_profile}
-    let!(:active_employer_staff_role) {FactoryGirl.build(:benefit_sponsor_employer_staff_role, aasm_state:'is_active', benefit_sponsor_employer_profile_id: employer_profile.id)}
+    let(:user) { FactoryGirl.create(:user)}
+    let(:person) { FactoryGirl.create(:person, emails:[FactoryGirl.build(:email, kind:'work')], user: user) }
+    let!(:active_employer_staff_role) {FactoryGirl.build(:benefit_sponsor_employer_staff_role, aasm_state:'is_active', benefit_sponsor_employer_profile_id: employer_profile.id, person: person)}
     let(:broker_agency) {FactoryGirl.create(:benefit_sponsors_organizations_general_organization, :with_broker_agency_profile, site: site)}
     let!(:broker_agency_profile) {broker_agency.broker_agency_profile}
-    let!(:broker_role) { FactoryGirl.create(:broker_role, aasm_state: 'active', benefit_sponsors_broker_agency_profile_id: broker_agency_profile.id) }
-    let(:person) { FactoryGirl.create(:person, emails:[FactoryGirl.build(:email, kind:'work')],employer_staff_roles:[active_employer_staff_role],broker_role:broker_role) }
-    let(:user) { FactoryGirl.create(:user, :person => person)}
+    let!(:broker_role) { FactoryGirl.create(:broker_role, aasm_state: 'active', benefit_sponsors_broker_agency_profile_id: broker_agency_profile.id, person: person) }
+    let!(:broker_agency_staff_role) { FactoryGirl.build(:broker_agency_staff_role, benefit_sponsors_broker_agency_profile_id: broker_agency_profile.id, person: person )}
 
     shared_examples_for "should return profile type" do |profile_type|
 
@@ -70,6 +71,30 @@ module BenefitSponsors
     describe ".find" do
       it_behaves_like "should find profile and return form for profile", "benefit_sponsor"
       it_behaves_like "should find profile and return form for profile", "broker_agency"
+    end
+
+    describe ".has_broker_agency_staff_role_for_profile" do
+      context "Person with Broker agency staff roles" do
+
+        it  "should return true if broker staff is assigned to a broker agency profile" do
+          params = { profile_id: broker_agency_profile.id, profile_type:"broker_agency_staff"}
+          service = subject.new params
+          expect(service.has_broker_agency_staff_role_for_profile(user, broker_agency_profile)). to eq true
+        end
+
+        it  "should return true if broker staff is assigned to a broker agency profile" do
+          params = { profile_id: broker_agency_profile.id, profile_type:"broker_agency_staff"}
+          service = subject.new params
+          expect(service.has_broker_agency_staff_role_for_profile(user, broker_agency_profile)). to eq true
+        end
+
+        it  "should return false if broker staff is not assigned to a broker agency profile" do
+          person.broker_agency_staff_roles.each{|staff| staff.update_attributes(benefit_sponsors_broker_agency_profile_id: nil)}
+          params = { profile_id: broker_agency_profile.id, profile_type:"broker_agency_staff"}
+          service = subject.new params
+          expect(service.has_broker_agency_staff_role_for_profile(user, broker_agency_profile)). to eq false
+        end
+      end
     end
   end
 end

naort-dev/enroll-ruby

components/benefit_sponsors/spec/models/benefit_sponsors/services/new_profile_registration_service_spec.rb
@@ -7,14 +7,15 @@ module BenefitSponsors
     subject { BenefitSponsors::Services::NewProfileRegistrationService }
     let!(:security_question)  { FactoryBot.create_default :security_question }
     let!(:site) { ::BenefitSponsors::SiteSpecHelpers.create_cca_site_with_hbx_profile_and_benefit_market }
-    let!(:general_org) {FactoryBot.create(:benefit_sponsors_organizations_general_organization, :with_aca_shop_cca_employer_profile, site: site)}
-    let!(:employer_profile) { general_org.employer_profile }
-    let(:broker_agency) {FactoryBot.create(:benefit_sponsors_organizations_general_organization, :with_broker_agency_profile, site: site)}
-    let!(:broker_agency_profile) {broker_agency.broker_agency_profile}
+    let!(:general_org) {FactoryGirl.create(:benefit_sponsors_organizations_general_organization, :with_aca_shop_cca_employer_profile, site: site)}
+    let!(:employer_profile) {general_org.employer_profile}
     let!(:user) { FactoryBot.create(:user)}
     let!(:person) { FactoryBot.create(:person, emails:[ FactoryBot.build(:email, kind:'work') ], user_id: user.id) }
     let!(:active_employer_staff_role) {FactoryBot.create(:benefit_sponsor_employer_staff_role, aasm_state:'is_active', benefit_sponsor_employer_profile_id: employer_profile.id, person: person)}
-    let!(:broker_role) { FactoryBot.create(:broker_role, aasm_state: 'active', benefit_sponsors_broker_agency_profile_id: broker_agency_profile.id) }
+    let!(:broker_role) { FactorBot.create(:broker_role, aasm_state: 'active', benefit_sponsors_broker_agency_profile_id: broker_agency_profile.id, person: person) }
+    let!(:broker_agency_staff_role) { FactoryBot.build(:broker_agency_staff_role, benefit_sponsors_broker_agency_profile_id: broker_agency_profile.id, person: person )}
+    let(:broker_agency) {FactoryGirl.create(:benefit_sponsors_organizations_general_organization, :with_broker_agency_profile, site: site)}
+    let!(:broker_agency_profile) {broker_agency.broker_agency_profile}
     let!(:general_agency) {FactoryBot.create(:benefit_sponsors_organizations_general_organization, :with_general_agency_profile, site: site)}
     let!(:general_agency_profile) {general_agency.profiles.first }
     let(:general_role) {FactoryBot.create(:general_agency_staff_role, aasm_state: "active", benefit_sponsors_general_agency_profile_id: general_agency_profile.id)}
@@ -90,6 +91,7 @@ module BenefitSponsors
       it_behaves_like "should find profile and return form for profile", "general_agency"
     end
 
+
     describe ".is_benefit_sponsor_already_registered?" do
       context "Should return when person found" do
         before :each do
@@ -110,6 +112,30 @@ module BenefitSponsors
         let(:general_agency_user) { FactoryBot.create(:user, :person => general_agency_person)}
         it "has general_agency_staff_role" do
           expect(subject.new.has_general_agency_staff_role_for_profile?(general_agency_user,general_agency_profile)).to eq false
+        end
+      end
+    end
+
+    describe ".has_broker_agency_staff_role_for_profile" do
+      context "Person with Broker agency staff roles" do
+
+        it  "should return true if broker staff is assigned to a broker agency profile" do
+          params = { profile_id: broker_agency_profile.id, profile_type:"broker_agency_staff"}
+          service = subject.new params
+          expect(service.has_broker_agency_staff_role_for_profile(user, broker_agency_profile)). to eq true
+        end
+
+        it  "should return true if broker staff is assigned to a broker agency profile" do
+          params = { profile_id: broker_agency_profile.id, profile_type:"broker_agency_staff"}
+          service = subject.new params
+          expect(service.has_broker_agency_staff_role_for_profile(user, broker_agency_profile)). to eq true
+        end
+
+        it  "should return false if broker staff is not assigned to a broker agency profile" do
+          person.broker_agency_staff_roles.each{|staff| staff.update_attributes(benefit_sponsors_broker_agency_profile_id: nil)}
+          params = { profile_id: broker_agency_profile.id, profile_type:"broker_agency_staff"}
+          service = subject.new params
+          expect(service.has_broker_agency_staff_role_for_profile(user, broker_agency_profile)). to eq false
         end
       end
     end