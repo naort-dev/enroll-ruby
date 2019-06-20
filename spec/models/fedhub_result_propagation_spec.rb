ideacrew/enroll

fedhub_result_propagation spec fix

spec/models/fedhub_result_propagation_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 
 describe "A new consumer role with an individual market enrollment", :dbclean => :around_each do
-  let(:person) { FactoryGirl.create(:person, :with_consumer_role) }
+  let(:person) { FactoryGirl.create(:person, :with_consumer_role, :with_active_consumer_role) }
   let(:family) { FactoryGirl.create(:individual_market_family, primary_person: person) }
   let(:hbx_profile) { FactoryGirl.create(:hbx_profile, :open_enrollment_coverage_period) }
   let(:enrollment) do
@@ -34,9 +34,9 @@ describe "A new consumer role with an individual market enrollment", :dbclean =>
       end
 
       it "sets is_any_enrollment_member_outstanding field to true" do
-          enroll = HbxEnrollment.by_hbx_id(enrollment.hbx_id).first
-          expect(enroll.aasm_state).to eql "coverage_selected"
-          expect(enroll.is_any_enrollment_member_outstanding).to eql true
+        enroll = HbxEnrollment.by_hbx_id(enrollment.hbx_id).first
+        expect(enroll.aasm_state).to eql "coverage_selected"
+        expect(enroll.is_any_enrollment_member_outstanding).to eql true
       end
     end
 
@@ -47,8 +47,8 @@ describe "A new consumer role with an individual market enrollment", :dbclean =>
       end
 
       it "does not change the state of the enrollment" do
-          enroll = HbxEnrollment.by_hbx_id(enrollment.hbx_id).first
-          expect(enroll.aasm_state).to eql "coverage_terminated"
+        enroll = HbxEnrollment.by_hbx_id(enrollment.hbx_id).first
+        expect(enroll.aasm_state).to eql "coverage_terminated"
       end
     end
   end

naort-dev/enroll-ruby

spec/models/fedhub_result_propagation_spec.rb
@@ -3,7 +3,7 @@ require "rails_helper"
 if ExchangeTestingConfigurationHelper.individual_market_is_enabled?
 
 describe "A new consumer role with an individual market enrollment", :dbclean => :around_each do
-  let(:person) { FactoryBot.create(:person, :with_consumer_role) }
+  let(:person) { FactoryBot.create(:person, :with_consumer_role, :with_active_consumer_role) }
   let(:family) { FactoryBot.create(:individual_market_family, primary_person: person) }
   let(:hbx_profile) { FactoryBot.create(:hbx_profile, :open_enrollment_coverage_period) }
   let(:product) { FactoryBot.create(:benefit_markets_products_health_products_health_product, benefit_market_kind: :aca_individual, kind: :health, csr_variant_id: '01') }
@@ -36,9 +36,9 @@ describe "A new consumer role with an individual market enrollment", :dbclean =>
       end
 
       it "sets is_any_enrollment_member_outstanding field to true" do
-          enroll = HbxEnrollment.by_hbx_id(enrollment.hbx_id).first
-          expect(enroll.aasm_state).to eql "coverage_selected"
-          expect(enroll.is_any_enrollment_member_outstanding).to eql true
+        enroll = HbxEnrollment.by_hbx_id(enrollment.hbx_id).first
+        expect(enroll.aasm_state).to eql "coverage_selected"
+        expect(enroll.is_any_enrollment_member_outstanding).to eql true
       end
     end
 
@@ -49,8 +49,8 @@ describe "A new consumer role with an individual market enrollment", :dbclean =>
       end
 
       it "does not change the state of the enrollment" do
-          enroll = HbxEnrollment.by_hbx_id(enrollment.hbx_id).first
-          expect(enroll.aasm_state).to eql "coverage_terminated"
+        enroll = HbxEnrollment.by_hbx_id(enrollment.hbx_id).first
+        expect(enroll.aasm_state).to eql "coverage_terminated"
       end
     end
   end