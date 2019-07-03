ideacrew/enroll

refs# 25391 PAY NOW Functionality available for all users except hbx_readonly_admin

spec/helpers/application_helper_spec.rb
@@ -504,4 +504,30 @@ end
       expect{helper.convert_to_bool(val9)}.to raise_error(ArgumentError)
     end
   end
+
+  describe "can_access_pay_now_button" do
+    let!(:person1){FactoryGirl.create(:person, user: user1)}
+    let!(:user1){FactoryGirl.create(:user)}
+    let!(:hbx_staff_role1) { FactoryGirl.create(:hbx_staff_role, person: person1, subrole: "hbx_staff", permission_id: permission.id)}
+    let!(:person2){FactoryGirl.create(:person, user: user2)}
+    let!(:user2){FactoryGirl.create(:user)}
+    let!(:hbx_staff_role2) { FactoryGirl.create(:hbx_staff_role, person: person2, subrole: "hbx_read_only", permission_id: permission.id)}
+    let!(:person3){FactoryGirl.create(:person, user: user3)}
+    let!(:user3){FactoryGirl.create(:user)}
+    let!(:permission) { FactoryGirl.create(:permission)}
+
+    it "should return true when hbx staff login as admin " do
+      a = user1.person.hbx_staff_role.permission
+      expect(a.can_access_pay_now).not_to eq true
+    end
+
+    it "should return false when hbx readonly login as admin " do
+      b = user2.person.hbx_staff_role.permission
+      expect(b.can_access_pay_now).to eq false
+    end
+
+    it "should return nil when there is no staff role for person " do
+      expect(user3.person.hbx_staff_role).to eq nil
+    end
+  end
 end

naort-dev/enroll-ruby

spec/helpers/application_helper_spec.rb
@@ -566,4 +566,30 @@ end
       expect{helper.convert_to_bool(val9)}.to raise_error(ArgumentError)
     end
   end
+
+  describe "can_access_pay_now_button" do
+    let!(:person1) { FactoryBot.create(:person, user: user1) }
+    let!(:user1) { FactoryBot.create(:user) }
+    let!(:hbx_staff_role1) { FactoryBot.create(:hbx_staff_role, person: person1, subrole: "hbx_staff", permission_id: permission.id)}
+    let!(:person2) { FactoryBot.create(:person, user: user2) }
+    let!(:user2) { FactoryBot.create(:user) }
+    let!(:hbx_staff_role2) { FactoryBot.create(:hbx_staff_role, person: person2, subrole: "hbx_read_only", permission_id: permission.id)}
+    let!(:person3) { FactoryBot.create(:person, user: user3) }
+    let!(:user3) { FactoryBot.create(:user) }
+    let!(:permission) { FactoryBot.create(:permission)}
+
+    it "should return true when hbx staff login as admin " do
+      a = user1.person.hbx_staff_role.permission
+      expect(a.can_access_pay_now).not_to eq true
+    end
+
+    it "should return false when hbx readonly login as admin " do
+      b = user2.person.hbx_staff_role.permission
+      expect(b.can_access_pay_now).to eq false
+    end
+
+    it "should return nil when there is no staff role for person " do
+      expect(user3.person.hbx_staff_role).to eq nil
+    end
+  end
 end