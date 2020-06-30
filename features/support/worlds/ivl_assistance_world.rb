ideacrew/enroll

Refs #87299, spec fixes

features/support/worlds/ivl_assistance_world.rb
@@ -9,17 +9,18 @@ module IvlAssistanceWorld
 
   def create_tax_household_and_eligibility_determination(family)
     tax_household = TaxHousehold.new(
-        effective_starting_on: (TimeKeeper.date_of_record - 30.days),
-        is_eligibility_determined: true,
-        submitted_at: TimeKeeper.date_of_record
-    )
+      effective_starting_on: (TimeKeeper.date_of_record - 30.days),
+      is_eligibility_determined: true,
+      submitted_at: TimeKeeper.date_of_record
+      )
     family.active_household.tax_households << tax_household
     family.active_household.save!
     tax_household.eligibility_determinations << EligibilityDetermination.new(
-        max_aptc: 100.00,
-        csr_percent_as_integer: 87,
-        determined_on: (TimeKeeper.date_of_record - 30.days)
-    )
+      source: 'Admin',
+      max_aptc: 100.00,
+      csr_percent_as_integer: 87,
+      determined_on: (TimeKeeper.date_of_record - 30.days)
+      )
     tax_household.save!
     family.save!
     tax_household
@@ -29,17 +30,17 @@ module IvlAssistanceWorld
     family = Family.all.first
     tax_household = create_tax_household_and_eligibility_determination(family)
     tax_household.tax_household_members << TaxHouseholdMember.new(
-        applicant_id: family.family_members[0].id,
-        is_subscriber: true,
-        is_ia_eligible: true,
-        is_medicaid_chip_eligible: false
-    )
+      applicant_id: family.family_members[0].id,
+      is_subscriber: true,
+      is_ia_eligible: true,
+      is_medicaid_chip_eligible: false
+      )
     tax_household.tax_household_members << TaxHouseholdMember.new(
-        applicant_id: family.family_members[1].id,
-        is_subscriber: false,
-        is_ia_eligible: true,
-        is_medicaid_chip_eligible: false
-    )
+      applicant_id: family.family_members[1].id,
+      is_subscriber: false,
+      is_ia_eligible: true,
+      is_medicaid_chip_eligible: false
+      )
     tax_household.save!
     family.active_household.save!
     family.save!
@@ -49,17 +50,17 @@ module IvlAssistanceWorld
     family = Family.all.first
     tax_household = create_tax_household_and_eligibility_determination(family)
     tax_household.tax_household_members << TaxHouseholdMember.new(
-        applicant_id: family.family_members[0].id,
-        is_subscriber: true,
-        is_ia_eligible: true,
-        is_medicaid_chip_eligible: false
-    )
+      applicant_id: family.family_members[0].id,
+      is_subscriber: true,
+      is_ia_eligible: true,
+      is_medicaid_chip_eligible: false
+      )
     tax_household.tax_household_members << TaxHouseholdMember.new(
-        applicant_id: family.family_members[1].id,
-        is_subscriber: false,
-        is_ia_eligible: false,
-        is_medicaid_chip_eligible: true
-    )
+      applicant_id: family.family_members[1].id,
+      is_subscriber: false,
+      is_ia_eligible: false,
+      is_medicaid_chip_eligible: true
+      )
     tax_household.save!
     family.active_household.save!
     family.save!

spec/data_migrations/reactivate_tax_household_with_assistance_year_spec.rb
@@ -17,7 +17,7 @@ describe ReactivateTaxHouseholdWithAssistanceYear, dbclean: :after_each do
     let(:person) { FactoryBot.create(:person) }
     let(:household) {Household.new}
     let(:family) { FactoryBot.create(:family, :with_primary_family_member, person: person, households: [household])}
-    let(:eligibility_determination) {EligibilityDetermination.new(csr_eligibility_kind: 'csr_73', max_aptc: 1017, csr_percent: 0.73, determined_on: TimeKeeper.date_of_record)}
+    let(:eligibility_determination) {EligibilityDetermination.new(source: 'Admin', csr_eligibility_kind: 'csr_73', max_aptc: 1017, csr_percent: 0.73, determined_on: TimeKeeper.date_of_record)}
     let(:tax_household) {TaxHousehold.new(effective_starting_on: TimeKeeper.date_of_record, effective_ending_on: TimeKeeper.date_of_record+1.year, eligibility_determinations: [eligibility_determination])}
 
     before(:each) do
@@ -48,7 +48,7 @@ describe ReactivateTaxHouseholdWithAssistanceYear, dbclean: :after_each do
     let(:person) { FactoryBot.create(:person) }
     let(:household) {Household.new}
     let(:family) { FactoryBot.create(:family, :with_primary_family_member, person: person, households: [household])}
-    let(:eligibility_determination) {EligibilityDetermination.new(csr_eligibility_kind: 'csr_73', max_aptc: 100, csr_percent: 0.73, determined_on: TimeKeeper.date_of_record)}
+    let(:eligibility_determination) {EligibilityDetermination.new(source: 'Admin', csr_eligibility_kind: 'csr_73', max_aptc: 100, csr_percent: 0.73, determined_on: TimeKeeper.date_of_record)}
     let(:tax_household) {TaxHousehold.new(effective_starting_on: TimeKeeper.date_of_record, effective_ending_on: TimeKeeper.date_of_record+1.year, eligibility_determinations: [eligibility_determination])}
 
     before(:each) do
@@ -78,7 +78,7 @@ describe ReactivateTaxHouseholdWithAssistanceYear, dbclean: :after_each do
     let(:person) { FactoryBot.create(:person) }
     let(:household) {Household.new}
     let(:family) { FactoryBot.create(:family, :with_primary_family_member, person: person, households: [household])}
-    let(:eligibility_determination) {EligibilityDetermination.new(csr_eligibility_kind: 'csr_87', max_aptc: 100, csr_percent: 0.87, determined_on: TimeKeeper.date_of_record)}
+    let(:eligibility_determination) {EligibilityDetermination.new(source: 'Admin', csr_eligibility_kind: 'csr_87', max_aptc: 100, csr_percent: 0.87, determined_on: TimeKeeper.date_of_record)}
     let(:tax_household) {TaxHousehold.new(effective_starting_on: TimeKeeper.date_of_record, effective_ending_on: TimeKeeper.date_of_record+1.year, eligibility_determinations: [eligibility_determination])}
 
     before(:each) do

naort-dev/enroll-ruby

features/support/worlds/ivl_assistance_world.rb
@@ -9,17 +9,18 @@ module IvlAssistanceWorld
 
   def create_tax_household_and_eligibility_determination(family)
     tax_household = TaxHousehold.new(
-        effective_starting_on: (TimeKeeper.date_of_record - 30.days),
-        is_eligibility_determined: true,
-        submitted_at: TimeKeeper.date_of_record
-    )
+      effective_starting_on: (TimeKeeper.date_of_record - 30.days),
+      is_eligibility_determined: true,
+      submitted_at: TimeKeeper.date_of_record
+      )
     family.active_household.tax_households << tax_household
     family.active_household.save!
     tax_household.eligibility_determinations << EligibilityDetermination.new(
-        max_aptc: 100.00,
-        csr_percent_as_integer: 87,
-        determined_at: (TimeKeeper.date_of_record - 30.days)
-    )
+      source: 'Admin',
+      max_aptc: 100.00,
+      csr_percent_as_integer: 87,
+      determined_at: (TimeKeeper.date_of_record - 30.days)
+      )
     tax_household.save!
     family.save!
     tax_household
@@ -29,17 +30,17 @@ module IvlAssistanceWorld
     family = Family.all.first
     tax_household = create_tax_household_and_eligibility_determination(family)
     tax_household.tax_household_members << TaxHouseholdMember.new(
-        applicant_id: family.family_members[0].id,
-        is_subscriber: true,
-        is_ia_eligible: true,
-        is_medicaid_chip_eligible: false
-    )
+      applicant_id: family.family_members[0].id,
+      is_subscriber: true,
+      is_ia_eligible: true,
+      is_medicaid_chip_eligible: false
+      )
     tax_household.tax_household_members << TaxHouseholdMember.new(
-        applicant_id: family.family_members[1].id,
-        is_subscriber: false,
-        is_ia_eligible: true,
-        is_medicaid_chip_eligible: false
-    )
+      applicant_id: family.family_members[1].id,
+      is_subscriber: false,
+      is_ia_eligible: true,
+      is_medicaid_chip_eligible: false
+      )
     tax_household.save!
     family.active_household.save!
     family.save!
@@ -49,17 +50,17 @@ module IvlAssistanceWorld
     family = Family.all.first
     tax_household = create_tax_household_and_eligibility_determination(family)
     tax_household.tax_household_members << TaxHouseholdMember.new(
-        applicant_id: family.family_members[0].id,
-        is_subscriber: true,
-        is_ia_eligible: true,
-        is_medicaid_chip_eligible: false
-    )
+      applicant_id: family.family_members[0].id,
+      is_subscriber: true,
+      is_ia_eligible: true,
+      is_medicaid_chip_eligible: false
+      )
     tax_household.tax_household_members << TaxHouseholdMember.new(
-        applicant_id: family.family_members[1].id,
-        is_subscriber: false,
-        is_ia_eligible: false,
-        is_medicaid_chip_eligible: true
-    )
+      applicant_id: family.family_members[1].id,
+      is_subscriber: false,
+      is_ia_eligible: false,
+      is_medicaid_chip_eligible: true
+      )
     tax_household.save!
     family.active_household.save!
     family.save!

spec/data_migrations/reactivate_tax_household_with_assistance_year_spec.rb
@@ -17,7 +17,7 @@ describe ReactivateTaxHouseholdWithAssistanceYear, dbclean: :after_each do
     let(:person) { FactoryBot.create(:person) }
     let(:household) {Household.new}
     let(:family) { FactoryBot.create(:family, :with_primary_family_member, person: person, households: [household])}
-    let(:eligibility_determination) {EligibilityDetermination.new(csr_eligibility_kind: 'csr_73', max_aptc: 1017, csr_percent: 0.73, determined_at: TimeKeeper.date_of_record)}
+    let(:eligibility_determination) {EligibilityDetermination.new(source: 'Admin', csr_eligibility_kind: 'csr_73', max_aptc: 1017, csr_percent: 0.73, determined_at: TimeKeeper.date_of_record)}
     let(:tax_household) {TaxHousehold.new(effective_starting_on: TimeKeeper.date_of_record, effective_ending_on: TimeKeeper.date_of_record+1.year, eligibility_determinations: [eligibility_determination])}
 
     before(:each) do
@@ -48,7 +48,7 @@ describe ReactivateTaxHouseholdWithAssistanceYear, dbclean: :after_each do
     let(:person) { FactoryBot.create(:person) }
     let(:household) {Household.new}
     let(:family) { FactoryBot.create(:family, :with_primary_family_member, person: person, households: [household])}
-    let(:eligibility_determination) {EligibilityDetermination.new(csr_eligibility_kind: 'csr_73', max_aptc: 100, csr_percent: 0.73, determined_at: TimeKeeper.date_of_record)}
+    let(:eligibility_determination) {EligibilityDetermination.new(source: 'Admin', csr_eligibility_kind: 'csr_73', max_aptc: 100, csr_percent: 0.73, determined_at: TimeKeeper.date_of_record)}
     let(:tax_household) {TaxHousehold.new(effective_starting_on: TimeKeeper.date_of_record, effective_ending_on: TimeKeeper.date_of_record+1.year, eligibility_determinations: [eligibility_determination])}
 
     before(:each) do
@@ -78,7 +78,7 @@ describe ReactivateTaxHouseholdWithAssistanceYear, dbclean: :after_each do
     let(:person) { FactoryBot.create(:person) }
     let(:household) {Household.new}
     let(:family) { FactoryBot.create(:family, :with_primary_family_member, person: person, households: [household])}
-    let(:eligibility_determination) {EligibilityDetermination.new(csr_eligibility_kind: 'csr_87', max_aptc: 100, csr_percent: 0.87, determined_at: TimeKeeper.date_of_record)}
+    let(:eligibility_determination) {EligibilityDetermination.new(source: 'Admin', csr_eligibility_kind: 'csr_87', max_aptc: 100, csr_percent: 0.87, determined_at: TimeKeeper.date_of_record)}
     let(:tax_household) {TaxHousehold.new(effective_starting_on: TimeKeeper.date_of_record, effective_ending_on: TimeKeeper.date_of_record+1.year, eligibility_determinations: [eligibility_determination])}
 
     before(:each) do