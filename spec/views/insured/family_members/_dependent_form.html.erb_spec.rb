vargasbo/enroll

refs #89068 spec fixes

spec/views/insured/family_members/_dependent_form.html.erb_spec.rb
@@ -37,14 +37,14 @@ describe "insured/family_members/_dependent_form.html.erb" do
     context "when individual market is enabled" do
       let(:individual_market_is_enabled) { true }
       it "should have consumer_fields area" do
-        expect(rendered).to have_css('#consumer_fields .row:first-child label', text: 'Are you a US citizen or US national?')
+        expect(rendered).to have_css('#consumer_fields .row:first-child label', text: 'Is this person a US Citizen or US National?')
         expect(rendered).to have_selector("div#consumer_fields")
-        expect(rendered).to match(/Are you a US citizen or US national/)
+        expect(rendered).to match(/Is this person a US citizen or US national/)
       end
 
       it "should have show tribal_container" do
         expect(rendered).to have_selector('div#tribal_container')
-        expect(rendered).to have_content('Are you a member of an American Indian or Alaskan Native tribe? *')
+        expect(rendered).to have_content('Is this person a member of an American Indian or Alaskan Native tribe? *')
       end
 
       it "should display the is_applying_coverage field option" do

naort-dev/enroll-ruby

spec/views/insured/family_members/_dependent_form.html.erb_spec.rb
@@ -37,14 +37,14 @@ describe "insured/family_members/_dependent_form.html.erb" do
     context "when individual market is enabled" do
       let(:individual_market_is_enabled) { true }
       it "should have consumer_fields area" do
-        expect(rendered).to have_css('#consumer_fields .row:first-child label', text: 'Are you a US citizen or US national?')
+        expect(rendered).to have_css('#consumer_fields .row:first-child label', text: 'Is this person a US Citizen or US National?')
         expect(rendered).to have_selector("div#consumer_fields")
-        expect(rendered).to match(/Are you a US citizen or US national/)
+        expect(rendered).to match(/Is this person a US citizen or US national/)
       end
 
       it "should have show tribal_container" do
         expect(rendered).to have_selector('div#tribal_container')
-        expect(rendered).to have_content('Are you a member of an American Indian or Alaska Native Tribe? *')
+        expect(rendered).to have_content('Is this person a member of an American Indian or Alaska Native Tribe? *')
       end
 
       it "should display the is_applying_coverage field option" do